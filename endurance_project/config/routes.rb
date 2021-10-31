7ddcc8e04e34308a7f834ab10da0594dfe2f1d82
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-10-31 18:52:37 -0400

    setup done

--- endurance_project/config/routes.rb
+++ endurance_project/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
    root "static_pages#root"
+    namespace :api, defaults: {format: :json} do
+    
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
