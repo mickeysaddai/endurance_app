c4971d5b4f256b4eec09c463e5adb98600309372
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-10-31 17:39:57 -0400

    setup done

--- endurance_project/webpack.config.js
+++ endurance_project/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    context: __dirname,
     entry: './frontend/endurance.jsx',
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
@@ -10,20 +9,17 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    query: {
-                        presets: ['@babel/env', '@babel/react']
-                    }
-                },
+                test: [/\.jsx?$/],
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env', '@babel/react']
+                }
             }
         ]
     },
     devtool: 'source-map',
     resolve: {
-        extensions: [".js", ".jsx", "*"]
+        extensions: ['.js', '.jsx', '*'],
     }
 };
-
