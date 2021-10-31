e581220ef596105c0dd3d8f4b688502c8a63b564
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-10-31 19:54:57 -0400

    session_actions

--- endurance_project/frontend/endurance.jsx
+++ endurance_project/frontend/endurance.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-
+import ReactDOM from "react-dom";
+import {logout} from "./util/session_api_util"
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+    window.logout = logout;
+  
     ReactDOM.render(<h1>Welcome to Endurance</h1>, root);
 });
