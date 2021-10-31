ddbda0615f596af455c215fc0c0585cd7dbbaf2b
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-10-31 19:56:42 -0400

    session action revised

--- endurance_project/frontend/actions/session_actions.js
+++ endurance_project/frontend/actions/session_actions.js
@@ -1,31 +1,41 @@
-import * as SessionApiUtil from "../util/session_api_util";
+import * as APIUtil from '../util/session_api_util';
 
-export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
-export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
-export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
-const receiveCurrentUser = currentUser => ({
+export const receiveCurrentUser = currentUser => ({
     type: RECEIVE_CURRENT_USER,
-    currentUser,
+    currentUser
 });
-const logoutCurrentUser = () => ({
+
+export const logoutCurrentUser = () => ({
     type: LOGOUT_CURRENT_USER,
 });
-const receiveErrors = errors => ({
-    type: RECEIVE_ERRORS,
-    errors,
+
+export const receiveErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
 });
 
-export const login = user =>
-    dispatch(
-        SessionApiUtil.login(user)
-            .then(user => dispatch(receiveCurrentUser(user)), errors => dispatch(receiveErrors(errors.responseJSON)))
-    );
-export const logout = () =>
-    dispatch(SessionApiUtil.logout())
-        .then(() => dispatch(logoutCurrentUser()), errors => dispatch(receiveErrors(errors.responseJSON)));
-export const signup = user =>
-    dispatch(
-        SessionApiUtil.signup(user)
-            .then(user => dispatch(receiveCurrentUser(user)), errors => dispatch(receiveErrors(errors.responseJSON)))
-    );
+export const signup = user => dispatch => (
+    APIUtil.signup(user).then(user => (
+        dispatch(receiveCurrentUser(user))
+    ), err => (
+        dispatch(receiveErrors(err.responseJSON))
+    ))
+);
+
+export const login = user => dispatch => (
+    APIUtil.login(user).then(user => (
+        dispatch(receiveCurrentUser(user))
+    ), err => (
+        dispatch(receiveErrors(err.responseJSON))
+    ))
+);
+
+export const logout = () => dispatch => (
+    APIUtil.logout().then(user => (
+        dispatch(logoutCurrentUser())
+    ))
+);
