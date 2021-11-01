a27477cbdee2938bed23a26177f9876684ef164b
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-10-31 22:45:15 -0400

    user_auth done

--- endurance_project/frontend/components/app.jsx
+++ endurance_project/frontend/components/app.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignUpFormContainer from "./session_form/signup_form_container";
 import { AuthRoute } from "../util/route_util";
+import { Switch } from "react-router";
+// import { Route, Redirect, withRouter } from 'react-router-dom';
 
 
 const App = () => (
     <div>
-        <header>
+        {/* <header>
             <h1>Endurance</h1>
-        </header>
-        <AuthRoute path='./login' component={LoginFormContainer} />
-        <AuthRoute path='./signup' component={SignUpFormContainer} />
+        </header> */}
+        <Switch>
+            <AuthRoute exact path='/login' component={LoginFormContainer} />
+            <AuthRoute exact path='/signup' component={SignUpFormContainer} />
+        </Switch>
     </div>
+
 );
 
 export default App;
