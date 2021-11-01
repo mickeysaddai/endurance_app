82f7d4c641295da971ec59798aaa6e1cee1f5e82
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-10-31 20:09:08 -0400

    root_reducer

---
+++ endurance_project/frontend/reducers/root_reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import entitiesReducer from './entities_reducer';
+import errorsReducer from './errorsReducer';
+import sessionReducer from './session_reducer';
+const rootReducer = combineReducers({
+    entities: entitiesReducer,
+    session: sessionReducer,
+    errors: errorsReducer
+})
+
+export default rootReducer;
