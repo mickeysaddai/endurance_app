e4b423fcb66d5a18413725d4fe222a420b981c51
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-10-31 20:21:38 -0400

    store done

--- endurance_project/frontend/endurance.jsx
+++ endurance_project/frontend/endurance.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {logout} from "./util/session_api_util"
-import {store} from "./store/store"
+import configureStore from "./store/store"
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     const store = configureStore();
     window.store = store
+    window.getState = store.getState;
    
     ReactDOM.render(<h1>Welcome to Endurance</h1>, root);
 
