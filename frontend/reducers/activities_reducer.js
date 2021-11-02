cb9fffdfa1301b4ec28cf886892755b083b00a97
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-11-02 17:02:48 -0400

    reducer done

--- frontend/reducers/activities_reducer.js
+++ frontend/reducers/activities_reducer.js
@@ -1,17 +1,23 @@
 import { RECEIVE_ACTIVITIES, RECEIVE_ACTIVITY, REMOVE_ACTIVITY } from "../actions/activity_actions";
 
 
-const ActivitiesReducer = (oldState, action) => {
+const ActivitiesReducer = (oldState = {}, action) => {
     Object.freeze(oldState);
     let nextState = Object.assign({}, oldState)
 
     switch (action.type) {
         case RECEIVE_ACTIVITIES:
-            return action.activities
+            return action.activities;
 
+        case RECEIVE_ACTIVITY:
+            nextState[action.activity.id] = action.activity
+            return nextState;
+        case REMOVE_ACTIVITY:
+            delete nextState[action.activityId]
+            return nextState;
         
         default:
-            break;
+            oldState;
     }
 
 }
