4046ba76d605d24cec52376db07d0ec352acb5ed
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-11-03 17:22:37 -0400

    activityform

---
+++ frontend/components/activities/activity_form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class ActivityForm extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+export default ActivityForm;
