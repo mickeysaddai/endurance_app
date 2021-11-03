283009f2cf6761c6426f524e78e9387f6d2eb1e9
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-11-03 17:11:09 -0400

    users reducer updated

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,11 +1,16 @@
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+import { RECEIVE_ALL_USERS, RECEIVE_USER, REMOVE_USER } from '../actions/user_actions';
 
 const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
-
+        case RECEIVE_ALL_USERS:
+            return Object.assign({}, action.users);
+        case RECEIVE_USER:
+            return Object.assign({}, state, action.user);
+        case REMOVE_USER:
         default:
             return state;
     }
