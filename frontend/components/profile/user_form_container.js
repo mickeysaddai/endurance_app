ee6407a67bc5ef0adfd2152b4148f44599d3cde7
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-11-23 14:06:47 -0500

    installed @oclif/command

--- frontend/components/profile/user_form_container.js
+++ frontend/components/profile/user_form_container.js
@@ -7,7 +7,8 @@ import { logout } from "../../actions/session_actions";
 const mapStateToProps = (state) => ({
     activities: Object.values(state.entities.activities),
     username: state.session.username,
-    userPhoto: "https://3znvnpy5ek52a26m01me9p1t-wpengine.netdna-ssl.com/wp-content/uploads/2017/07/noimage_person.png" 
+    // userPhoto: "https://3znvnpy5ek52a26m01me9p1t-wpengine.netdna-ssl.com/wp-content/uploads/2017/07/noimage_person.png" 
+    userPhoto: "https://scontent-iad3-2.xx.fbcdn.net/v/t1.6435-9/126910317_164782895294381_8016587112177111609_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=vfjYD2elIpUAX8Ej4Wc&_nc_ht=scontent-iad3-2.xx&oh=f22782f3ab2aa86d9dba6a1ba73c3de2&oe=61B22A34" 
 
 
 })
