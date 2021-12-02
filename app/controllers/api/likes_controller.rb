8824fc294ab86370e4bae3024f8c057212569b64
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-12-02 11:12:45 -0500

    removed lines 10 and 11 from .gitignore

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,2 +1,15 @@
 class Api::LikesController < ApplicationController
+      before_action :find_activity
+      
+    def create
+        @activity.likes.create(user_id: current_user.id)
+        render: show
+    end
+
+      private
+  def find_activity
+    @activity= Activity.find(params[:activity_id])
+  end
+
+
 end
