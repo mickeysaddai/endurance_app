0960fa046e8719c46d64f5bebb890ceb552c27aa
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2021-12-14 13:29:18 -0500

    fiexed total stats

--- frontend/components/profile/user_form_container.js
+++ frontend/components/profile/user_form_container.js
@@ -5,7 +5,8 @@ import { logout } from "../../actions/session_actions";
 
 
 const mapStateToProps = (state) => {
-    const allActivities = Object.values(state.entities.activities);
+    // const allActivities = Object.values(state.entities.activities);
+    const allActivities =  Object.values(state.entities.activities).filter(activity => activity.user_id === state.session.id);
     let totalMiles = 0;
     let totalCalories = 0;
 
@@ -21,7 +22,7 @@ const mapStateToProps = (state) => {
     return {
     activities: Object.values(state.entities.activities),
     username: state.session.username,
-    totalMiles: totalMiles,
+    totalMiles: totalMiles.toFixed(2),
     totalCalories: totalCalories,
     allActivities: allActivities.length,
     userPhoto: "https://3znvnpy5ek52a26m01me9p1t-wpengine.netdna-ssl.com/wp-content/uploads/2017/07/noimage_person.png" 
